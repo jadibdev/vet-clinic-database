7692334c28d7f3069d245f918d932330c33fe480
Mohamed Jadib <jadibdev@gmail.com>
2022-08-09 15:13:28 +0100

    Move scripts to query file

--- queries.sql
+++ queries.sql
@@ -1,3 +1,44 @@
+ALTER TABLE animals
+ADD Species varchar(255);
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name like '%mon'
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
+
 select * from animals where name like '%mon';
 select date_of_birth from animals where name ='Agumon' or name = 'Pikachu';
 select * from animals where neutered = True;
