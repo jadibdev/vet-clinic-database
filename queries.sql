4440c2b33df660343004f990986f1b25355869b9
Mohamed Jadib <jadibdev@gmail.com>
2022-08-09 14:36:06 +0100

    Update queries, schema and data

--- queries.sql
+++ queries.sql
@@ -6,3 +6,16 @@ select name, escape_attemps from animals where weight_kg >10.5;
 select name from animals where neutered = True and escape_attemps < 3;
 select * from animals where weight_kg between 10.4 and 17.3;
 select name from animals where  extract(year from date_of_birth) between 2016 and 2019;
+
+SELECT COUNT(name) FROM animals;
+SELECT COUNT(escape_attemps) FROM animals
+WHERE escape_attemps = 0;
+SELECT AVG(weight_kg)
+FROM animals;
+SELECT neutered, SUM(escape_attemps) FROM animals 
+GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg)  FROM animals 
+GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN 'January 1, 1990' AND 'December 31, 2000'
+GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,9 +1,51 @@
 /* Database schema to keep the structure of entire database. */
 
- CREATE TABLE animals (
+CREATE TABLE animals (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(100), 
     date_of_birth DATE, 
     escape_attemps INT, 
     neutered BOOLEAN, 
-    weight_kg DECIMAL);
+    weight_kg DECIMAL,
+    species VARCHAR(100)
+    );
+    
+ALTER TABLE animals
+ADD Species varchar(255);
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name like '%mon'
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
