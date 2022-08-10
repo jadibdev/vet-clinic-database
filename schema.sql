9502978269189f48eb7d7cba1aa7ccc5b455a731
Mohamed Jadib <jadibdev@gmail.com>
2022-08-10 19:49:38 +0100

    Create owners table, create species table, update data and queries files

--- schema.sql
+++ schema.sql
@@ -1,11 +1,23 @@
-/* Database schema to keep the structure of entire database. */
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INT
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  species_name VARCHAR(100)
+);
 
 CREATE TABLE animals (
-    id BIGSERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100), 
     date_of_birth DATE, 
     escape_attemps INT, 
     neutered BOOLEAN, 
     weight_kg DECIMAL,
-    species VARCHAR(100)
-    );
+    species_id INT REFERENCES species(id),
+    owner_id INT REFERENCES owners(id)
+);
+
+
