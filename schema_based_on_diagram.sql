d9b4330ef4f90ed9499bf211ac7ad9e318c4d03b
whid-bb <whid11s50@gmail.com>
2022-08-16 17:28:38 +0200

    add tables
    treatments, invoiceItems, join table medicalHistories_treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -19,3 +19,26 @@ CREATE TABLE Invoices(
 );
 CREATE INDEX medical_history__ids ON Invoices (medical_history__id);
 
+CREATE TABLE Treatments(
+  id INT PRIMARY KEY,
+  type VARCHAR(225),
+  name VARCHAR(255)
+);
+CREATE TABLE InvoiceItems(
+  id INT PRIMARY KEY,
+  unit_price FLOAT(4),
+  quantity INT,
+  total_price FLOAT(4),
+  invoice_id INT REFERENCES Invoices(id) ON DELETE CASCADE,
+  treatment_id INT REFERENCES Treatments (id) ON DELETE CASCADE
+);
+CREATE INDEX invoice_ids ON InvoiceItems (invoice_id);
+CREATE INDEX treatment_ids ON InvoiceItems (treatment_id);
+-- Join tables 
+CREATE TABLE MedicalHistories_Treatments (
+  id INT PRIMARY KEY,
+  medical_history_id INT REFERENCES MedicalHistories(id) ON DELETE CASCADE,
+  treatment_id INT REFERENCES Treatments(id) ON DELETE CASCADE
+);
+CREATE INDEX medical_history_ids ON MedicalHistories_Treatments (medical_history_id);
+CREATE INDEX join_treatment_ids ON MedicalHistories_Treatments (treatment_id);
